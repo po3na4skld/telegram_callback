8580eaf1c3688e240e33568336ede0c84a163c49
Kirill Chernov <36486349+po3na4skld@users.noreply.github.com>
2019-04-24 14:24:16 +0300

    Add files via upload

---
+++ telegram_callback.py
@@ -0,0 +1,35 @@
+from keras.callbacks import Callback
+import requests
+
+
+class TelegramCallback(Callback):
+
+    def __init__(self, username):
+        super(TelegramCallback, self).__init__()
+        self.token = '777234481:AAERTylbFtBOhF1wfmXTht0O3JwRqXcoiAk'
+        res = requests.get("https://api.telegram.org/bot{}/getUpdates".format(self.token))
+        jsn = res.json()['result']
+        for updt in jsn:
+            if updt['message']['from']['username'] == username:
+                self.chat_id = updt['message']['chat']['id']
+
+    def send_info(self, text):
+        requests.post("https://api.telegram.org/bot{}/sendMessage".format(self.token),
+                      data={'chat_id': self.chat_id, 'text': text})
+
+    def on_train_begin(self, logs=None):
+        if requests is None:
+            raise ImportError('Notification requires the requests library.')
+        text = 'Training successfully started.'
+        self.send_info(text)
+
+    def on_train_end(self, logs=None):
+        text = 'Training successfully ended.'
+        self.send_info(text)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        text = 'Epoch {}: '.format(epoch + 1)
+        for metric in self.params['metrics']:
+            text += '{}: {:.3f} '.format(metric, logs[metric])
+        self.send_info(text)
